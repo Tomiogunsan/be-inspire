4d7a0ceeccd28e50e4bf9323cf418e58385ea2e7
Tomi Ogunsan <tomiafolayan@gmail.com>
2023-06-11 17:33:15 +0100

    feat: implemented reusable input component

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,19 @@
-import Button from "@/Sharedcomponents/button/Button";
-
-
+import Button from '@/Sharedcomponents/button/Button'
+import Input from '@/Sharedcomponents/input/input'
 
 function App() {
   return (
     <div>
-      <Button type="button" variant="primary">Click me</Button>
-      <Button as="a" variant="link">Click me</Button>
+      <Button type='button' variant='primary'>
+        Click me
+      </Button>
+      <Button as='a' variant='link'>
+        Click me
+      </Button>
+      <Input type='password' label='password' placeholder='Password' />
+      <Input type='input' label='Email ' placeholder='Email' />
     </div>
   )
 }
 
-export default App;
+export default App
