329777bd6671b12d577a2b0a51915279a3ff8690
Tomi Ogunsan <tomiafolayan@gmail.com>
2023-06-11 13:57:43 +0100

    feat: implemented reuseable button component

--- src/components/button/Button.tsx
+++ src/components/button/Button.tsx
@@ -1,8 +1,34 @@
 import React from 'react'
 import { PolymorphicComponentProps } from './button.type'
+import clsx from 'clsx'
 
-export default function Button() {
+type ButtonProps<C extends React.ElementType> = PolymorphicComponentProps<
+  C,
+  {
+    children: React.ReactNode
+    className?: string
+    variant: 'primary' | 'link'
+  }
+>
+
+const btnClasses = 'px-4 py-3 transition duration-300'
+
+const VARIANTS = {
+  primary: 'bg-blue-900 text-blue-100 hover:bg-blue-700',
+  link: 'text-indigo-700 hover:text-indigo-800 hover:border-b border-indigo-800 !px-0 !pb-1',
+}
+
+export default function Button<C extends React.ElementType = 'button'>(
+  props: ButtonProps<C>
+) {
+  const { children, as, className, variant, ...buttonProps } = props
+  const Component = as || 'button'
   return (
-    <div>Button</div>
+    <Component
+      className={clsx(className, btnClasses, variant && VARIANTS[variant])}
+      {...buttonProps}
+    >
+      {children}
+    </Component>
   )
 }
