ac260a847598a60a462d32b6dc55cc5c1c3f2949
Tomi Ogunsan <tomiafolayan@gmail.com>
2023-06-15 20:10:20 +0100

    feat: implemented dashboard Layout and outlet

--- src/layout/components/Sidebar.tsx
+++ src/layout/components/Sidebar.tsx
@@ -4,15 +4,15 @@ import { BsPencilSquare } from 'react-icons/bs'
 
 export default function Sidebar() {
   return (
-    <div className='flex bg-[#fff] w-[20%] h-screen border-r-[3px]'>
-      <div className='flex flex-col p-4 mt-16'>
-        <div className='flex items-center gap-2 text-lg mb-6 text-[#535a65]'>
-          <AiFillHome className='text-[#000001] text-xl' />
+    <div className='flex bg-[#fff] w-[20%] h-screen '>
+      <div className='flex flex-col p-4 mt-28  cursor-pointer '>
+        <div className='flex items-center gap-4 text-lg mb-6 text-[#535a65] ml-6 hover:bg-[#5d53f1] hover:text-[#f4f4f4] hover:px-4 hover:py-4 hover:rounded-md hover:transition  ease-linear'>
+          <AiFillHome className='text-[#000001] text-xl hover:text-[#f4f4f4]' />
           Home
         </div>
-        <div className='flex items-center justify-center gap-2 text-lg text-[#535a65]'>
-          <BsPencilSquare className=' text-xl text-[#000001]' />
-          Write a Quote
+        <div className=' ml-6 flex items-center justify-center gap-4 text-lg text-[#535a65] hover:bg-[#5d53f1] hover:text-[#f4f4f4] hover:px-4 hover:py-4 hover:rounded-md hover:transition  ease-linear'>
+          <BsPencilSquare className=' text-xl text-[#000001] hover:text-[#f4f4f4]' />
+          Create Quote
         </div>
       </div>
     </div>
