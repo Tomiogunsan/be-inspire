4ba3add46503cef7091b2a6ae5f5d66a898dd2d7
Tomi Ogunsan <tomiafolayan@gmail.com>
2023-06-20 11:04:10 +0100

    feat: styled the quote

--- src/components/Quote.tsx
+++ src/components/Quote.tsx
@@ -5,9 +5,11 @@ export default function Quote(props: QuoteProps) {
   const { firstName, quote } = props
 //   console.log(firstName)
   return (
-    <div>
-      <p>{firstName}</p>
-      <p>{quote}</p>
-    </div>
+    
+      <div className='bg-red-600 rounded-md text-white flex flex-col items-center justify-center'>
+        <p>{firstName}</p>
+        <p>{quote}</p>
+      </div>
+   
   )
 }
--- src/views/dashboard/ViewQuote.tsx
+++ src/views/dashboard/ViewQuote.tsx
@@ -20,9 +20,13 @@ export default function ViewQuote(props: QuoteProps) {
   const { data } = useQuery(FEED_QUERY)
   //   console.log(data)
   return (
-    <div>
+    <div className=' grid grid-cols-3 gap-4 mt-20 ml-8'>
       {data?.feed?.links?.map((quote: QuoteProps) => (
-        <Quote key={quote.id} firstName={quote?.firstName} quote={quote?.quote} />
+        <Quote
+          key={quote.id}
+          firstName={quote?.firstName}
+          quote={quote?.quote}
+        />
       ))}
     </div>
   )
